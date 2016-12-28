0fdec8aef03d4d2cd9ffb3b9ba34d8233d6022a6
Brad Stell <bradstell87@gmail.com>
2016-12-28 16:52:28 -0500

    Through textures, onto vec & mat math

---
+++ opengl_1_glfw/Texture.cpp
@@ -0,0 +1,181 @@
+//#include <iostream>
+//
+//// GLEW
+//#define GLEW_STATIC
+//#include <GL/glew.h>
+//
+//// GLFW
+//#include <GLFW/glfw3.h>
+//
+//// SOIL
+//#include <SOIL.h>
+//
+//#include "shader.h"
+//
+//// Function prototypes
+//void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+//
+//// Window dimensions
+//const GLuint WIDTH = 800, HEIGHT = 600;
+//
+//// The MAIN function, from here we start the application and run the game loop
+//int main()
+//{
+//	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+//
+//	// Init GLFW
+//	glfwInit();
+//
+//	// Set all the required options for GLFW
+//	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+//
+//	// Create a GLFWwindow object that we can use for GLFW's functions
+//	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Tutorial 1 - GLFW", nullptr, nullptr);
+//	glfwMakeContextCurrent(window);
+//
+//	glfwSetKeyCallback(window, key_callback);
+//
+//	glewExperimental = GL_TRUE;
+//	glewInit();
+//
+//	int width, height;
+//	glfwGetFramebufferSize(window, &width, &height);
+//	glViewport(0, 0, width, height);
+//
+//	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	// Read and compile shaders
+//	Shader shader("texture.vs", "texture.frag");
+//
+//
+//	GLfloat vertices[] = {
+//		// Positions         // Colors           // Texture Coords
+//		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   2.0f, 2.0f,   // Top Right
+//		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   2.0f, 0.0f,   // Bottom Right
+//		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Bottom Left
+//		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f    // Top Left 
+//	};
+//	GLuint indices[] = {
+//		0, 1, 3,
+//		1, 2, 3
+//	};
+//
+//	GLuint VAO, VBO, EBO;
+//	glGenVertexArrays(1, &VAO);
+//	glGenBuffers(1, &VBO);
+//	glGenBuffers(1, &EBO);
+//
+//	glBindVertexArray(VAO);
+//	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+//
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+//	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+//
+//	// position attribute
+//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+//	glEnableVertexAttribArray(0);
+//	// Color attribute
+//	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+//	glEnableVertexAttribArray(1);
+//	// Position attribute
+//	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+//	glEnableVertexAttribArray(2);
+//
+//	glBindVertexArray(0);
+//	glBindBuffer(GL_ARRAY_BUFFER, 0);
+//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+//
+//
+//	// Load texture and create
+//	GLuint texture1, texture2;
+//	glGenTextures(1, &texture1);
+//	glGenTextures(1, &texture2);
+//	// Bind current texture to OpenGL
+//	glBindTexture(GL_TEXTURE_2D, texture1);
+//	// Set the texture wrapping parameters
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT (usually basic wrapping method)
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+//	// Set texture filtering parameters
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//
+//
+//	// Load image, create texture and generate mipmaps
+//	int texwidth, texheight;
+//	unsigned char* image = SOIL_load_image("container.jpg", &texwidth, &texheight, 0, SOIL_LOAD_RGB);
+//	// Create the texture
+//	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texwidth, texheight, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+//	glGenerateMipmap(GL_TEXTURE_2D);
+//	// Free image memory and unbind texture object
+//	SOIL_free_image_data(image);
+//	glBindTexture(GL_TEXTURE_2D, 0);
+//
+//	glBindTexture(GL_TEXTURE_2D, texture2);
+//	// Set the texture wrapping parameters
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);	// Set texture wrapping to GL_REPEAT (usually basic wrapping method)
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+//	// Set texture filtering parameters
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+//	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//
+//	int texwidth2, texheight2;
+//	unsigned char* image2 = SOIL_load_image("awesomeface.png", &texwidth2, &texheight2, 0, SOIL_LOAD_RGB);
+//	// Create the texture
+//	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texwidth2, texheight2, 0, GL_RGB, GL_UNSIGNED_BYTE, image2);
+//	glGenerateMipmap(GL_TEXTURE_2D);
+//	// Free image memory and unbind texture object
+//	SOIL_free_image_data(image2);
+//	glBindTexture(GL_TEXTURE_2D, 0);
+//
+//	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	// Game loop
+//	while (!glfwWindowShouldClose(window))
+//	{
+//		glfwPollEvents();
+//
+//		// ============================================
+//
+//		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+//		glClear(GL_COLOR_BUFFER_BIT);
+//
+//		shader.Use();
+//
+//		glActiveTexture(GL_TEXTURE0);
+//		glBindTexture(GL_TEXTURE_2D, texture1);
+//		glUniform1i(glGetUniformLocation(shader.Program, "ourTexture1"), 0);
+//		glActiveTexture(GL_TEXTURE1);
+//		glBindTexture(GL_TEXTURE_2D, texture2);
+//		glUniform1i(glGetUniformLocation(shader.Program, "ourTexture2"), 1);
+//
+//		glBindVertexArray(VAO);
+//		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+//		glBindVertexArray(0);
+//
+//		// ===========================================
+//
+//		glfwSwapBuffers(window);
+//	}
+//
+//	// Terminate GLFW, clearing any resources allocated by GLFW.
+//	glDeleteVertexArrays(1, &VAO);
+//	glDeleteBuffers(1, &VBO);
+//	glDeleteBuffers(1, &EBO);
+//	glBindTexture(GL_TEXTURE_2D, 0);
+//	glfwTerminate();
+//	return 0;
+//}
+//
+//// Is called whenever a key is pressed/released via GLFW
+//void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+//{
+//	std::cout << key << std::endl;
+//	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+//		glfwSetWindowShouldClose(window, GL_TRUE);
+//	else
+//		std::cout << "You pressed key: " << key << std::endl;
+//}
--- opengl_1_glfw/shader.h
+++ opengl_1_glfw/shader.h
@@ -72,7 +72,7 @@ Shader::Shader(const GLchar* vertexPath, const GLchar* fragmentPath)
 	};
 
 	//  Fragment Shader
-	fragment = glCreateShader(GL_VERTEX_SHADER);
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragment, 1, &fShaderCode, NULL);
 	glCompileShader(fragment);
 	// Print compile errors if any
